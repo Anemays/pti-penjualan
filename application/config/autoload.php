fd71e91ed86b5978cafc5c2c95b77a2510d1c51d
Ra <68149490+Anemays@users.noreply.github.com>
2024-11-11 23:23:04 +0700

    Downgrade PHP to 7.1.33
    XAMPP 3.2.4

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+#$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'form_validation', 'pdf', 'session', 'email', 'PHPExcel');
 
 /*
 | -------------------------------------------------------------------
